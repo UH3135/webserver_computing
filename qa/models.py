7c94b4cad322dbdcd081464c6b87f3f65eef29fa
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-03-30 16:04:36 +0900

    FEAT: add like and unlike function

--- qa/models.py
+++ qa/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    like = models.IntegerField()
-    unlike = models.IntegerField()
+    like = models.IntegerField(default=0)
+    unlike = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     create_date = models.DateTimeField(null=True)
 
