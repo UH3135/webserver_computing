c6e6b24e5bca7ba8e4ddf71ad5c50bbecf97bb8d
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-03-30 16:49:35 +0900

    FIX: add slash to questions-detail url

--- qa/urls.py
+++ qa/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('questions/', views.QuestionListAPIView.as_view(), name='question_list'),
     path('questions/new/', views.QuestionCreateAPIView.as_view(), name='question_update'),
-    path('questions/<int:pk>', views.QuestionDetailAPIView.as_view(), name='question_detail'),
+    path('questions/<int:pk>/', views.QuestionDetailAPIView.as_view(), name='question_detail'),
     path('questions/<int:pk>/edit', views.QuestionUpdateAPIView.as_view(), name='question_create'),
     path('questions/<int:pk>/delete', views.QuestionDeleteAPIView.as_view(), name='question_delete'),
     # path('login/', auth_views.LoginView.as_view(template_name='qa/login.html'), name='login'),
