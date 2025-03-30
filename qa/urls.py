a40a0c7390547028fa6b05fd990a643812fed545
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-03-30 16:30:23 +0900

    FEAT: create api/qa/question

--- qa/urls.py
+++ qa/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
-from django.contrib.auth import views as auth_views
+# from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    path('', views.question_list),
-    path('questions/<int:pk>', views.question_detail, name="question-detail"),
-    path('ask/', views.ask_question, name="ask-question"),
-    path('login/', auth_views.LoginView.as_view(template_name='qa/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='qa/logout.html'), name='logout')
+    path('questions/', views.QuestionListAPIView.as_view(), name='question_list'),
+    path('questions/new/', views.QuestionCreateAPIView.as_view(), name='question_update'),
+    path('questions/<int:pk>', views.QuestionDetailAPIView.as_view(), name='question_detail'),
+    path('questions/<int:pk>/edit', views.QuestionUpdateAPIView.as_view(), name='question_create'),
+    path('questions/<int:pk>/delete', views.QuestionDeleteAPIView.as_view(), name='question_delete'),
+    # path('login/', auth_views.LoginView.as_view(template_name='qa/login.html'), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(template_name='qa/logout.html'), name='logout')
 ]
--- qa/views.py
+++ qa/views.py
@@ -1,42 +1,28 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from rest_framework import generics
+from .serializer import QuestionSerializer, AnswerSerializer
 from .models import Question, Answer
-from django.contrib.auth.decorators import login_required
-from django.utils import timezone
-from django.contrib.auth.models import User
 
 
-def question_list(request):
-    questions = Question.objects.order_by('-create_date')
-    return render(request, 'qa/question_list.html', {'questions': questions})
+class QuestionCreateAPIView(generics.CreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
 
-def question_detail(request, pk):
-    question = get_object_or_404(Question, pk=pk)
-    if request.method == 'POST':
-        content = request.POST.get('content')
-        if content and request.user.is_authenticated:
-            Answer.objects.create(
-                question=question,
-                content=content,
-                author=request.user,
-                created_at=timezone.now()
-            )
-        return redirect('question_detail', pk=pk)
-    return render(request, 'qa/question_detail.html', {'question': question})
+class QuestionListAPIView(generics.ListAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
 
-@login_required
-def ask_question(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
+class QuestionDetailAPIView(generics.RetrieveAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
-        if title and content:
-            Question.objects.create(
-                title=title,
-                content=content,
-                author=request.user,
-                created_at=timezone.now()
-            )
-        return redirect('question_list')
-    return render(request, 'qa/ask_question.html')
+
+class QuestionUpdateAPIView(generics.UpdateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+
+class QuestionDeleteAPIView(generics.DestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
