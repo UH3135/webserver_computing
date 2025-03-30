d2ab5aaa0acec46dd1b6de7ed57875738bf465fc
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-03-30 16:01:47 +0900

    FEAT: add like and unlike

--- qa/models.py
+++ qa/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    like = models.IntegerField()
+    unlike = models.IntegerField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     create_date = models.DateTimeField(null=True)
 
