07c3859c8aa6f2f5abc8d992e21cb14bfacde25a
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-04-21 14:26:19 +0900

    FEAT: Basic setting

--- config/settings.py
+++ config/settings.py
@@ -38,11 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
-    'rest_framework_simplejwt.token_blacklist',
-    'accounts',
-    'qa',
 ]
 
 MIDDLEWARE = [
@@ -127,20 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
-
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
-}
--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/qa/', include('qa.urls')),
-    path('api/auth/', include('accounts.urls')),
 ]
