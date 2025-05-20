2292d04705e4a63893bc86b039e37c8865f11baa
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-05-20 16:12:08 +0900

    FEAT: Book history 추가

--- library/urls.py
+++ library/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'library'
 
 urlpatterns = [
-    path('books/', views.book_list, name='book_list')
+    path('books/', views.book_list, name='book_list'),
+    path('books/<int:book_id>', views.book_history, name='book_history')
 ]
--- library/views.py
+++ library/views.py
@@ -4,4 +4,12 @@ from library.services import book_service
 
 def book_list(request):
     books = book_service.get_all_books()
-    return render(request, 'library/book_list.html', {'book': books})
+    return render(request, 'library/book_list.html', {'books': books})
+
+def book_history(request, book_id):
+    book = book_service.get_book_by_id(book_id)
+    histories = book_service.get_borrow_history_for_book(book)
+    return render(request, 'library/book_history.html', {
+        'book': book,
+        'histories': histories
+    })
